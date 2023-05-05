f3235201da2258a98b4ae62961f85d812360079c
maiyama <myam0118@gmail.com>
2023-05-06 07:33:03 +0900

    Fix Divider design

--- Sources/UI/Components/EpisodeDivider.swift
+++ Sources/UI/Components/EpisodeDivider.swift
@@ -5,8 +5,8 @@ public struct EpisodeDivider: View {
 
     public var body: some View {
         Divider()
-            .frame(height: 0.75)
-            .overlay { Color.secondary }
+            .frame(height: 0.5)
+            .overlay { Color(.systemGray2) }
             .padding(.vertical, 8)
     }
 }
